ac17a8047529b902ea8f4f8bc072ca658da8a384
YueTruong <tienphuc01012004@gmail.com>
2026-01-17 14:07:46 +0700

    fix(api): update baseURL to fallback to backend URL if API URL is not set

--- client/app/services/api.ts
+++ client/app/services/api.ts
@@ -4,7 +4,7 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || process.env.NEXT_PUBLIC_BACKEND_URL,
 });
 
 api.interceptors.request.use(async (config) => {
